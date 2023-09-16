260cc2e3797bc7d45b55ec95fb56e0950fb060f3
Raphael Lima <phaelslima@gmail.com>
2023-09-15 21:59:14 -0300

    feat: add sign in screen

---
+++ src/screens/SignIn.tsx
@@ -0,0 +1,52 @@
+import { useState } from 'react'
+import { Heading, Icon, useTheme, VStack } from 'native-base'
+import { Envelope, Key } from 'phosphor-react-native'
+
+import { Input } from '../components/Input'
+import { Button } from '../components/Button'
+
+import Logo from '../assets/logo_primary.svg'
+
+export function SignIn() {
+  const [email, setEmail] = useState('')
+  const [password, setPassword] = useState('')
+
+  const { colors } = useTheme()
+
+  function handleSignIn() {
+    console.log(email, password)
+  }
+
+  return (
+    <VStack flex={1} alignItems="center" bg="gray.600" px={8} pt={24}>
+      <Logo />
+
+      <Heading color="gray.100" fontSize="xl" mt={20} mb={6}>
+        Acesse sua conta
+      </Heading>
+
+      <Input
+        mb={4}
+        placeholder="E-mail"
+        InputLeftElement={
+          <Icon as={<Envelope color={colors.gray[300]} />} ml={4} />
+        }
+        autoCapitalize="none"
+        keyboardType="email-address"
+        onChangeText={setEmail}
+        value={email}
+      />
+
+      <Input
+        mb={8}
+        placeholder="Senha"
+        InputLeftElement={<Icon as={<Key color={colors.gray[300]} />} ml={4} />}
+        secureTextEntry
+        onChangeText={setPassword}
+        value={password}
+      />
+
+      <Button title="Entrar" w="full" onPress={handleSignIn} />
+    </VStack>
+  )
+}
