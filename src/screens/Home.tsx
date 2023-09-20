dbc87894ec1620bc8ae73b4dba259d677a8ef35d
Raphael Lima <phaelslima@gmail.com>
2023-09-19 21:26:48 -0300

    feat: implementing sign out method

--- src/screens/Home.tsx
+++ src/screens/Home.tsx
@@ -1,4 +1,5 @@
 import { useState } from 'react'
+import { Alert } from 'react-native'
 import {
   Center,
   FlatList,
@@ -11,6 +12,8 @@ import {
 } from 'native-base'
 import { useNavigation } from '@react-navigation/native'
 import { ChatTeardropText, SignOut } from 'phosphor-react-native'
+
+import auth from '@react-native-firebase/auth'
 
 import { Button } from '../components/Button'
 import { Filter } from '../components/Filter'
@@ -35,6 +38,14 @@ export function Home() {
     navigation.navigate('details', { orderId })
   }
 
+  function handleLogout() {
+    auth()
+      .signOut()
+      .catch((error) => {
+        return Alert.alert('Sair', 'Não foi possível sair.')
+      })
+  }
+
   return (
     <VStack flex={1} pb={6} bg="gray.700">
       <HStack
@@ -48,7 +59,10 @@ export function Home() {
       >
         <Logo />
 
-        <IconButton icon={<SignOut size={26} color={colors.gray[300]} />} />
+        <IconButton
+          icon={<SignOut size={26} color={colors.gray[300]} />}
+          onPress={handleLogout}
+        />
       </HStack>
 
       <VStack flex={1} px={6}>
